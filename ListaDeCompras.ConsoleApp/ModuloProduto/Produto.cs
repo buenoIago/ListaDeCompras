6bb2f38742801f7d07b90f2603f0cfc1f2021b88
buenoIago <iagocedup@gmail.com>
2026-05-04 15:49:36 -0300

    Adicionado Modulo de Produtos

---
+++ ListaDeCompras.ConsoleApp/ModuloProduto/Produto.cs
@@ -0,0 +1,52 @@
+using System;
+using ListaDeCompras.ConsoleApp.Compartilhado;
+using ListaDeCompras.ConsoleApp.ModuloCategoria;
+
+namespace ListaDeCompras.ConsoleApp.ModuloProduto;
+
+public class Produto : EntidadeBase
+{
+    public string Nome { get; private set; }
+    public string UnidadeMedida { get; private set; }
+    public decimal PrecoAproximado { get; private set; }
+    public Categoria Categoria { get; private set; }
+
+    public Produto(
+        string nome,
+        string unidadeMedida,
+        decimal precoAproximado,
+        Categoria categoria
+    )
+    {
+        Nome = nome;
+        UnidadeMedida = unidadeMedida;
+        PrecoAproximado = precoAproximado;
+        Categoria = categoria;
+    }
+
+    public override List<string> Validar()
+    {
+        List<string> erros = new List<string>();
+
+        if (Nome.Length < 2 || Nome.Length > 100)
+            erros.Add("O campo \"Nome\" deve conter entre 2 e 100 caracteres.");
+
+        if (string.IsNullOrWhiteSpace(UnidadeMedida))
+            erros.Add("O campo \"Unidade de Medida\" deve ser preenchido.");
+
+        if (PrecoAproximado == 0)
+            erros.Add("O campo \"Preço Aproximado\" deve ser preenchido.");
+
+        return erros;
+    }
+
+    public override void AtualizarDados(EntidadeBase entidadeAtualizada)
+    {
+        Produto produtoAtualizado = (Produto)entidadeAtualizada;
+
+        Nome = produtoAtualizado.Nome;
+        UnidadeMedida = produtoAtualizado.UnidadeMedida;
+        PrecoAproximado = produtoAtualizado.PrecoAproximado;
+        Categoria = produtoAtualizado.Categoria;
+    }
+}
---
+++ ListaDeCompras.ConsoleApp/ModuloProduto/TelaProduto.cs
@@ -0,0 +1,117 @@
+using System;
+using ListaDeCompras.ConsoleApp.Compartilhado;
+using ListaDeCompras.ConsoleApp.ModuloCategoria;
+
+namespace ListaDeCompras.ConsoleApp.ModuloProduto;
+
+public class TelaProduto : TelaBase<Produto>, ITelaOpcoes, ITelaCrud
+{
+    private readonly RepositorioCategoria repositorioCategoria;
+
+    public TelaProduto(
+        RepositorioProduto repositorioProduto,
+        RepositorioCategoria repositorioCategoria
+    ) : base("Produto", repositorioProduto)
+    {
+        this.repositorioCategoria = repositorioCategoria;
+    }
+
+    public override void VisualizarTodos(bool deveExibirCabecalho)
+    {
+        if (deveExibirCabecalho)
+            ExibirCabecalho("Visualização de Produtos");
+
+        List<Produto> produtos = repositorio.SelecionarTodos();
+
+        Console.WriteLine(
+            "{0, -7} | {1, -30} | {2, -15} | {3, -20} | {4, -15}",
+            "Id", "Nome", "Medida", "Preço Aproximado", "Categoria"
+        );
+
+        foreach (Produto p in produtos)
+        {
+            Console.WriteLine(
+                "{0, -7} | {1, -30} | {2, -15} | {3, -20} | {4, -15}",
+                p.Id, p.Nome, p.UnidadeMedida, p.PrecoAproximado.ToString("C2"), p.Categoria.Nome
+            );
+        }
+
+        if (deveExibirCabecalho)
+        {
+            Console.WriteLine("---------------------------------");
+            Console.Write("Digite ENTER para continuar...");
+            Console.ReadLine();
+        }
+    }
+
+    protected override Produto ObterDadosCadastrais()
+    {
+        Console.Write("Digite o nome do produto: ");
+        string nome = Console.ReadLine() ?? string.Empty;
+
+        Console.Write("Digite a unidade de medida do produto (ex: 2 lt, 5 kg): ");
+        string unidadeMedida = Console.ReadLine() ?? string.Empty;
+
+        Console.Write("Digite o preço aproximado do produto em R$: ");
+        decimal precoAproximado = Convert.ToDecimal(Console.ReadLine());
+
+        Categoria? categoriaSelecionada;
+
+        do
+        {
+            Console.WriteLine("---------------------------------");
+            VisualizarCategorias();
+            Console.WriteLine("---------------------------------");
+
+            Console.Write("Digite o Id da categoria do produto: ");
+            string idSelecionado = Console.ReadLine() ?? string.Empty;
+
+            categoriaSelecionada = repositorioCategoria.SelecionarPorId(idSelecionado);
+
+        } while (categoriaSelecionada == null);
+
+        return new Produto(nome, unidadeMedida, precoAproximado, categoriaSelecionada);
+    }
+
+    private void VisualizarCategorias()
+    {
+        List<Categoria> categorias = repositorioCategoria.SelecionarTodos();
+
+        if (categorias.Count == 0)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Não existe nenhum registro.");
+            Console.ResetColor();
+            Console.WriteLine("---------------------------------");
+            Console.Write("Digite ENTER para continuar...");
+            Console.ReadLine();
+            return;
+        }
+
+        Console.WriteLine(
+            "{0, -7} | {1, -20} | {2, -10}",
+            "Id", "Nome", "Cor"
+        );
+
+        foreach (Categoria c in categorias)
+        {
+            CorCategoria corSelecionada = c.Cor;
+
+            if (corSelecionada == CorCategoria.Vermelha)
+                Console.ForegroundColor = ConsoleColor.Red;
+
+            else if (corSelecionada == CorCategoria.Verde)
+                Console.ForegroundColor = ConsoleColor.Green;
+
+            else if (corSelecionada == CorCategoria.Azul)
+                Console.ForegroundColor = ConsoleColor.Blue;
+
+            Console.WriteLine(
+                "{0, -7} | {1, -20} | {2, -10}",
+                c.Id, c.Nome, c.Cor
+            );
+        }
+
+        Console.ResetColor();
+    }
+}
